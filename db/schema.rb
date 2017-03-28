b8e585a882ce2fb3405476468d0e4b739f7f9d4f
Magons <vano47@bk.ru>
2017-03-28 23:01:28 +0300

    Fix git ignore

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170328130753) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
@@ -45,4 +46,5 @@ ActiveRecord::Schema.define(version: 20170328130753) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
+
 end
