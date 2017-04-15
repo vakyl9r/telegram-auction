25b38044554fa48913a06d84f223f1e83877b883
Alex Sukov <sukovsasha@gmail.com>
2017-04-15 12:09:42 +0300

    Fix of routing 13

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,5 +1,5 @@
 class AuctionsController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   before_action :set_channel, only: [:new, :edit, :show, :index]
   before_action :set_auction, except: [:index, :new, :create]
 
